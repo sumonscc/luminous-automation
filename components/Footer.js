db1ada467488d05e47aee16c2437bad15d210108
sumonscc <49619668+sumonscc@users.noreply.github.com>
2025-08-13 21:47:18 +0600

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#003366',
+      color: 'white',
+      textAlign: 'center',
+      padding: '1rem',
+      marginTop: '2rem'
+    }}>
+      <p>© 2025 Luminous Automation. All rights reserved.</p>
+    </footer>
+  );
+}
